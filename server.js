46cdf143bc8c3786734af2a7f723a95aa377af29
Sanel Nezirevic <s.nezirevic@hotmail.com>
2021-10-05 15:30:20 +0200

    Initial Author

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
 if(process.env.NODE_ENV !== 'production') {
-require('dotenv').parse()
+
+ require('dotenv').config()
 }
 
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 const { use } = require('./routes/index')
+console.log("Database URL "+ process.env.DATABASE_URL);
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true })
-    const db = mongoose.connection
-    db.on('error', error => console.error(error))
-    db.once('open', () => console.log('Connected to Mongoose'))
+  
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', indexRouter)
+'authors/new'
 
 app.listen(process.env.PORT || 3000)
