8d92a4747721653cd2219acfeff50d0e626104c6
Sanel Nezirevic <s.nezirevic@hotmail.com>
2021-10-11 12:13:19 +0200

    Finish Book Routes

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 if(process.env.NODE_ENV !== 'production') {
-
- require('dotenv').config()
+require('dotenv').config()
 }
 
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const methodOverride = require('method-override')
 
 
 const indexRouter = require('./routes/index')
@@ -16,6 +16,7 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(express.urlencoded({ limit: '10mb', extended: false }))
 
